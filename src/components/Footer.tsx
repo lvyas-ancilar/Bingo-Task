337abba894b6ab207bb8d97bca7efbf2e0b0acbc
Lavesh Vyas <lvyas@ancilar.com>
2026-02-25 01:25:22 +0530

    feat: Update the footer ,change the year to 2026

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  const currentYear = 2026;
+  return (
+    <footer>
+      <p>&copy; {currentYear} Bingo Task</p>
+    </footer>
+  );
+};
+
+export default Footer;
