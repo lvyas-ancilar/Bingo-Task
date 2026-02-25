dda89b407dbfd6b240115bef1f4bfd9e4b3865cd
Lavesh Vyas <lvyas@ancilar.com>
2026-02-25 16:13:29 +0530

    feat: Create a nav bar component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from './components/NavBar';
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>Welcome to the app!</h1>
+    </div>
+  );
+};
+
+export default App;
