498a7eb6445cddea7f29003cb4e390f5c60805ff
Lavesh Vyas <lvyas@ancilar.com>
2026-02-25 11:40:16 +0530

    feat: /

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,13 +3,16 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
-  solidity: "0.8.28",
-  networks: {
-    hardhat: {
-      chainId: 31337,
+    solidity: "0.8.28",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
     },
-  },
-  etherscan: {
-    apiKey: "YOUR_ETHERSCAN_API_KEY",
-  },
+    etherscan: {
+        apiKey: "YOUR_ETHERSCAN_API_KEY",
+    },
+    contracts: [
+        "contracts/Bingo.sol",
+    ],
 };
